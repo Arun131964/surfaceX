3413f19775e4168fbde67eec7de992a214971f07
Arun131964 <bharathaarunkumar100@gmail.com>
2026-05-07 23:01:43 +0530

    Update vuln_scan.py

--- modules/vuln_scan.py
+++ modules/vuln_scan.py
@@ -1,8 +1,32 @@
+import requests
+
+payload = "<script>alert(1)</script>"
+
 def find_params(urls):
-    vulnerable = []
+    params = []
 
     for url in urls:
         if "=" in url and not any(ext in url for ext in [".js", ".css", ".png", ".jpg", ".svg"]):
-            vulnerable.append(url)
+            params.append(url)
+
+    return params
+
+
+def test_xss(urls):
+    vulnerable = []
+
+    print("[*] Testing for XSS...")
+
+    for url in urls:
+        try:
+            test_url = url.split("=")[0] + "=" + payload
+            r = requests.get(test_url, timeout=3)
+
+            if payload in r.text:
+                print(f"[VULN] XSS found: {test_url}")
+                vulnerable.append(test_url)
+
+        except:
+            continue
 
     return vulnerable
